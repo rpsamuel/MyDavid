6b0d63416b9a949ccc61ee26e752c149ced005fc
rpsamuel <roderic.samuel@ebitscorp.com>
2017-10-19 23:04:53 -0400

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -14,24 +14,5 @@ allert () { echo -e "${RED}$1${NC}"; }
 # Building project
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j8
-# Checks if last comand didn't output 0
-# $? checks what last command outputed
-# If output is 0 then command is succesfuly executed
-# If command fails it outputs number between 0 to 255
-if [ $? -ne 0 ]; then
-    error "Error: there are compile errors!"
-	# Terminate script and outputs 3
-    exit 3
-fi
-
-showinfo "Running tests ..."
-make -j8 Example_coverage
-ctest
-if [ $? -ne 0 ]; then
-    error "Error: there are failed tests!"
-    exit 4
-fi
-
-workingprocess "All tests compile and pass."
+ls -al
+pwd
